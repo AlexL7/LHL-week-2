7fc0fd2a74531a4d5876accc6e1f8b4d1f2f094e
Alexander <alexander.lock7@gmail.com>
2016-10-11 20:40:46 +0000

    finished random generator

--- tinyapp/express_server.js
+++ tinyapp/express_server.js
@@ -1,11 +1,17 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+'use strict';
 
-var urlDatabase = {
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded());
+
 
 app.set("view engine", "ejs");
 
@@ -18,6 +24,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id };
   res.render("urls_show", templateVars);
@@ -27,6 +37,16 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body.longURL);  // debug statement to see POST parameters
+  const rand = generateRandomString();      // Respond with 'Ok' (we will replace this)
+  urlDatabase[rand] = req.body.longURL;
+  res.redirect(`/urls/${rand}`);
+});
+
+
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
@@ -34,3 +54,14 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+function generateRandomString (){
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ ){
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
+    return text;
+}
